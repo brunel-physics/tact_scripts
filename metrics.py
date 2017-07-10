99f7142cf7f6567eafd124c83b6aab0d2fe20997
Corin <lunik1@users.noreply.github.com>
2017-07-10 15:34:35 +0100

    Split functions handling metrics into seperate file

---
+++ metrics.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+
+from scipy.stats import ks_2samp
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+def print_metrics(df_train, df_test, training_vars, mva):
+    """
+    Print metrics for a trained classifier
+    
+    Parameters
+    ----------
+    df_test : DataFrame
+        DataFrame containing testing data.
+    df_train: DataFrame
+        DataFrame containing training data.
+    training_vars : array_like
+        Names of features on which the classifier was trained.
+    mva : trained classifier
+        Classifier trained on df_train
+
+    Returns
+    -------
+    None
+    """
+
+    try:
+        test_prediction = mva.predict(df_test[training_vars])
+        train_prediction = mva.predict(df_train[training_vars])
+    except KeyError:
+        test_prediction = mva.predict(df_test[training_vars].as_matrix())
+        train_prediction = mva.predict(df_train[training_vars].as_matrix())
+
+    print("Classification Reports")
+    print("Test sample:")
+    print(classification_report(df_test.Signal, test_prediction,
+                                target_names=["background", "signal"]))
+    print("Training sample:")
+    print(classification_report(df_train.Signal, train_prediction,
+                                target_names=["background", "signal"]))
+
+    print("Confusion matrix:")
+    print("Test sample:")
+    print(confusion_matrix(df_test.Signal, test_prediction))
+    print("Training sample:")
+    print(confusion_matrix(df_train.Signal, train_prediction))
+    print()
+
+    print("KS Test p-value")
+    print("Signal:")
+    print(ks_2samp(df_train[df_train.Signal == 1].MVA,
+                   df_test[df_test.Signal == 1].MVA)[1])
+    print("Background:")
+    print(ks_2samp(df_train[df_train.Signal == 0].MVA,
+                   df_test[df_test.Signal == 0].MVA)[1])
+    print()
+
+    try:
+        print("Variable importance:")
+        for var, importance in sorted(
+                zip(training_vars, mva.feature_importances_),
+                key=lambda x: x[1],
+                reverse=True):
+            print("{0:15} {1:.3E}".format(var, importance))
+    except AttributeError:
+        pass
+    print()
--- mva_analysis.py
+++ mva_analysis.py
@@ -6,56 +6,9 @@ import errno
 import rootIO
 import preprocessing
 import classifiers
+import metrics
 import plotting as pt
-from scipy.stats import ks_2samp
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-
-
-def print_metrics(df_train, df_test, training_vars, mva):
-    """Print some basic metrics for the trained mva"""
-
-    try:
-        test_prediction = mva.predict(df_test[training_vars])
-        train_prediction = mva.predict(df_train[training_vars])
-    except KeyError:
-        test_prediction = mva.predict(df_test[training_vars].as_matrix())
-        train_prediction = mva.predict(df_train[training_vars].as_matrix())
-
-    print("Classification Reports")
-    print("Test sample:")
-    print(classification_report(df_test.Signal, test_prediction,
-                                target_names=["background", "signal"]))
-    print("Training sample:")
-    print(classification_report(df_train.Signal, train_prediction,
-                                target_names=["background", "signal"]))
-
-    print("Confusion matrix:")
-    print("Test sample:")
-    print(confusion_matrix(df_test.Signal, test_prediction))
-    print("Training sample:")
-    print(confusion_matrix(df_train.Signal, train_prediction))
-    print()
-
-    print("KS Test p-value")
-    print("Signal:")
-    print(ks_2samp(df_train[df_train.Signal==1].MVA,
-                   df_test[df_test.Signal==1].MVA)[1])
-    print("Background:")
-    print(ks_2samp(df_train[df_train.Signal==0].MVA,
-                   df_test[df_test.Signal==0].MVA)[1])
-    print()
-
-    try:
-        print("Variable importance:")
-        for var, importance in sorted(
-                zip(training_vars, mva.feature_importances_),
-                key=lambda x: x[1],
-                reverse=True):
-            print("{0:15} {1:.3E}".format(var, importance))
-    except AttributeError:
-        pass
-    print()
 
 
 def makedirs(*paths):
@@ -231,7 +184,7 @@ def main():
     df_train = classifiers.evaluate_mva(df_train, mva, training_vars)
 
     # Metrics
-    print_metrics(df_train, df_test, training_vars, mva)
+    metrics.print_metrics(df_train, df_test, training_vars, mva)
 
     pt.make_response_plot(df_train[df_train.Signal == 1],
                           df_test[df_test.Signal == 1],
