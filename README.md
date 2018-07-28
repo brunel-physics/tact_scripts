7794400f3d2a9edd91f82c60318d9aea4620fb52
Alexander <alexander.david.morton@cern.ch>
2018-07-28 10:56:10 +0100

    Added bare bones setup for minimal environment users on pion

---
+++ README.md
@@ -0,0 +1,6 @@
+By default pion uses python 2.6. If you do not have python 2.7 enabled by default or have root>=6.06.08 enabled, run
+```bash
+source setup.sh
+```
+in order to enable these.
+
---
+++ setup.sh
@@ -0,0 +1,3 @@
+export PATH=/scratch/shared/sw/Python/2.7.13/x86_64-slc6-gcc62-opt/bin:$PATH
+source /scratch/shared/sw/ROOT/6.06.08/x86_64-slc6-gcc62-opt/bin/thisroot.sh
+source /scratch/shared/sw/gcc/x86_64-slc6-gcc62-opt/setup.sh
